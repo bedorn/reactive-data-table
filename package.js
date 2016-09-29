0ec650c2943eb5519f1261b943ef6e1b06c4a245
Benjamin Dorner <b.dorner@wksgruppe.de>
2016-09-29 13:31:07 +0200

    version is package json

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'bedorn:reactive-data-table',
-    version: '1.0.6',
+    version: '1.0.7',
     summary: 'Reactive data table, styled with AdminLTE including pagination, filters and reactive translation',
     documentation: 'README.md',
     git: 'https://github.com/bedorn/reactive-data-table.git'
