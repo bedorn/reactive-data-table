38a205bc83ededbe05e0fd3bd7510466195b9eff
Benjamin Dorner <b.dorner@wksgruppe.de>
2018-02-23 10:39:27 +0100

    allow array as collection param

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'bedorn:reactive-data-table',
-    version: '1.2.2',
+    version: '1.2.3',
     summary: 'Reactive data table, styled with AdminLTE including pagination, filters and reactive translation',
     documentation: 'README.md',
     git: 'https://github.com/bedorn/reactive-data-table.git'
